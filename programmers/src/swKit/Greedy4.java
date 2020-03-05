d007fac9876a589add7b0a37272a25999c360736
isemang <a55306039@gmail.com>
2020-03-05 10:43:11 +0900

    [2020.03.05] 업데이트- 박민진 - 프로그래머스 SWkit 탐욕법 4번: 구명보트 문제 품

--- programmers/src/swKit/Greedy4.java
+++ programmers/src/swKit/Greedy4.java
@@ -1,5 +1,7 @@
 package swKit;
 
+import java.util.Arrays;
+
 //구명보트
 public class Greedy4 {
 
@@ -8,30 +10,26 @@ public class Greedy4 {
 		int[] p1 = {70, 50, 80, 50};
 		int[] p2 = {70, 80, 50};
 		int[] p3 = {20, 50, 50, 80};
+		int[] p4 = {20, 30, 30, 50, 70, 80, 90};
 		System.out.println(solution(p1,100));
 		System.out.println(solution(p2,100));
 		System.out.println(solution(p3,100));		//2
+		System.out.println(solution(p4,100));
 	}
 	
 	static int solution(int[] people, int limit) {
-        int answer = 0, boatW = 0, left = people.length, boatP = 0;
-        //boatW = 보트에 있는 무게, left=사람이 남아있는 수, i=index, boatP = 보트에 탄 사람 수
-        int[] rescue = new int[people.length];
-        while(left>0) {
-        	for (int i=answer; i<people.length; i++) {
-        		if(rescue[i]==0 && boatW+people[i]<=limit) {
-        			boatP++;
-        			boatW=people[i]+boatW;
-        			rescue[i]=1;
-        			left--;
-        		}
-        		if(boatP==2) break;
-        		else if(boatW == limit) break;
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int i=0, j;
+        for (j = people.length-1; i<j; j--) {
+        	if(people[i]+people[j]>limit) answer++;
+        	else {
+        		answer++;
+        		i++;
         	}
-        	answer++;
-        	boatP=0;
-        	boatW=0;
         }
+        if(i==j) answer++;
         
         return answer;
     }
